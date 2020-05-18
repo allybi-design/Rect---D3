29ca911464464fe6dfaea52ea0b54177155bc8ad
node-toso-api <alisonsmithaccy@gmail.com>
2020-05-18 16:57:05 +0100

    create bar graph with tooltips & add new Data

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { select, scaleBand, axisBottom, scaleLinear, axisLeft } from "d3";
 const App = () => {
   const svgRef = useRef()
 
-  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75, 40, 20, 45])
+  const [data, setData] = useState([3, 25, 30, 45, 60, 30, 45, 60])
 
   useEffect(() => {
     const svg = select(svgRef.current)
 
     const xScale = scaleBand()
-      .domain(data
-        .map((value, index) => index))
+      .domain(data.map((value, index) => index))
       .range([0, 300])
       .padding(0.5)
 
@@ -24,11 +23,13 @@ const App = () => {
       .range(["green", "yellow", "orange", "red"])
       .clamp(true)
 
+    //x-axis 
     svg
       .select(".x-axis")
       .style("transform", "translateY(150px)")
       .call(axisBottom(xScale).ticks(data.length))
 
+    //y-axis 
     svg
       .selectAll(".y-axis")
       // .style("transform", "translateX(300px")
@@ -38,15 +39,33 @@ const App = () => {
       .selectAll(".bar")
       .data(data)
       .join("rect")
-      .attr("class", "rect")
+      .attr("class", "bar")
       .style("transform", "scale(1, -1)")
       .attr("x", (value, index) => xScale(index))
       .attr("y", -150)
       .attr("width", xScale.bandwidth())
+      .on("mouseenter", (value, index) => {
+        svg
+          .selectAll(".tooltip")
+          .data([value])
+          .join(enter => enter.append("text").attr("y", yScale(value) - -8))
+          .attr("class", "tooltip")
+          .text(value)
+          .attr("x", xScale(index) + xScale.bandwidth() / 2)
+          .attr("text-anchor", "middle")
+          .transition()
+          .attr("y", yScale(value) - 8)
+          .attr("opacity", 1)
+      })
+      .on("mouseleave", () => svg.select(".tooltip").remove())
       .transition()
       .attr("fill", colorScale)
       .attr("height", value => 150 - yScale(value))
   }, [data])
+
+  const addNewData = () => {
+    setData([...data, Math.round((Math.random() * 100))])
+  }
 
   return (
     <div className="container">
@@ -54,7 +73,8 @@ const App = () => {
         <g className="x-axis" />
         <g className="y-axis" />
       </svg>
-      <button onClick={() => setData(data.map(value => value + 5))}>inc data by 5</button>
+      <button onClick={() => setData(data.map(value => value + 5))}>Inc data by 5</button>
+      <button onClick={() => addNewData()}>Add New Data</button>
     </div>
   )
 }
