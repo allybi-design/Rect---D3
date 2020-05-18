14c8e9c8f5c38663d70f9009968be7f2e9aec6a3
node-toso-api <alisonsmithaccy@gmail.com>
2020-05-18 15:10:49 +0100

    Adding Axies to a grid

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { select, line, curveCardinal } from "d3";
+import { select, line, curveCardinal, axisBottom, scaleLinear, axisLeft } from "d3";
 
 const App = () => {
   const svgRef = useRef()
-  
-  const [data, setData] = useState([25,30,45,60,20, 65, 75, 90])
+
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75, 40, 20, 45])
 
   useEffect(() => {
     const svg = select(svgRef.current)
-    const myLine = line()
-      .x((value, index) => index*50)
-      .y(value => 150-value)
-      .curve(curveCardinal)
-    // svg
-    //   .selectAll("circle")
-    //   .data(data)
-    //   .join("circle")
-    //   .attr("r", value => value)
-    //   .attr("cx", value => value * 4)
-    //   .attr("cy", value => value * 2)
-    //   .attr("stroke", "red")
+
+    const xScale = scaleLinear().domain([0, data.length - 1]).range([0, 300])
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0])
+
     svg
-      .selectAll("path")
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1))
+
+    svg
+      .selectAll(".y-axis")
+      // .style("transform", "translateX(300px")
+      .call(axisLeft(yScale))
+
+    const myLine = line()
+      .x((value, index) => xScale(index))
+      .y(yScale)
+      .curve(curveCardinal)
+  
+    svg
+      .selectAll(".line")
       .data([data])
       .join("path")
-      .attr("d", value => myLine(value))
+      .attr("class", "line")
+      .attr("d", myLine)
       .attr("fill", "none")
       .attr("stroke", "red")
-  },[data])
+  }, [data])
 
   return (
     <div className="container">
-      <h1>hello to D3 tut</h1>
-      <svg ref={svgRef}></svg>
-      <button onClick={() => setData(data.map(value => value+5))}>inc data by 5</button>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+      <button onClick={() => setData(data.map(value => value + 5))}>inc data by 5</button>
     </div>
   )
 }
