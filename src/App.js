7e5f6a8dee8ef9678e89dfedb435c39ff8043fc1
node-toso-api <alisonsmithaccy@gmail.com>
2020-06-06 14:42:43 +0100

    stacked bar chart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,81 @@
-import React, { useState } from 'react';
-import BrushChart from "./BrushChart"
-import BrushChartChild from "./BrushChartChild"
+import React, { useState } from "react";
+import StackedBarChart from "./StackedBarChart";
+import "./index.css";
+
+const data = [
+  {
+    year: 1980,
+    "🥑": 30,
+    "🍌": 10,
+    "🍆": 15
+  },
+  {
+    year: 1990,
+    "🥑": 20,
+    "🍌": 40,
+    "🍆": 60
+  },
+  {
+    year: 2000,
+    "🥑": 30,
+    "🍌": 45,
+    "🍆": 80
+  },
+  {
+    year: 2010,
+    "🥑": 40,
+    "🍌": 60,
+    "🍆": 100
+  },
+  {
+    year: 2020,
+    "🥑": 50,
+    "🍌": 80,
+    "🍆": 120
+  }
+];
+
+const allKeys = ["🥑", "🍌", "🍆"];
+
+const colors = {
+  "🥑": "green",
+  "🍌": "orange",
+  "🍆": "purple"
+};
 
 const App = () => {
 
-  const [data, setData] = useState(
-    Array.from({ length: 30 }).map(() => Math.round(Math.random() * 100))
-  )
+  const [keys, setKeys] = useState(allKeys);
 
   return (
     <div className="container">
-      <h1>Sub-selection with d3-brush</h1>
-      <BrushChart data={data}>
-        {selection => <BrushChartChild data={data} selection={selection} />}
-      </BrushChart>
-      <button onClick={() => setData([...data, Math.round(Math.random() * 100)])}>Add Data Node</button>
+
+      <h2>Stacked Bar Chart with D3 </h2>
+      <StackedBarChart data={data} keys={keys} colors={colors} />
+
+      <div className="fields">
+        {allKeys.map(key => (
+          <div key={key} className="field">
+            <input
+              id={key}
+              type="checkbox"
+              checked={keys.includes(key)}
+              onChange={e => {
+                if (e.target.checked) {
+                  setKeys(Array.from(new Set([...keys, key])));
+                } else {
+                  setKeys(keys.filter(_key => _key !== key));
+                }
+              }}
+            />
+            <label htmlFor={key} style={{ color: colors[key] }}>
+              {key}
+            </label>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
