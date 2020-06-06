74cd1a5b9555469a9053c83d10d8756e97db0334
node-toso-api <alisonsmithaccy@gmail.com>
2020-06-06 15:19:17 +0100

    Stacked Area Chart & Stacked Bar Chart

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
 import React, { useState } from "react";
 import StackedBarChart from "./StackedBarChart";
+import StackedAreaChart from "./StackedAreaChart";
 import "./index.css";
 
-const data = [
-  {
-    year: 1980,
-    "🥑": 30,
-    "🍌": 10,
-    "🍆": 15
-  },
-  {
-    year: 1990,
-    "🥑": 20,
-    "🍌": 40,
-    "🍆": 60
-  },
-  {
-    year: 2000,
-    "🥑": 30,
-    "🍌": 45,
-    "🍆": 80
-  },
-  {
-    year: 2010,
-    "🥑": 40,
-    "🍌": 60,
-    "🍆": 100
-  },
-  {
-    year: 2020,
-    "🥑": 50,
-    "🍌": 80,
-    "🍆": 120
-  }
-];
+
 
 const allKeys = ["🥑", "🍌", "🍆"];
 
@@ -45,14 +15,47 @@ const colors = {
 
 const App = () => {
 
+  const [data, setData] = useState([
+    {
+      year: 1980,
+      "🥑": 30,
+      "🍌": 10,
+      "🍆": 15
+    },
+    {
+      year: 1990,
+      "🥑": 20,
+      "🍌": 40,
+      "🍆": 60
+    },
+    {
+      year: 2000,
+      "🥑": 30,
+      "🍌": 45,
+      "🍆": 80
+    },
+    {
+      year: 2010,
+      "🥑": 40,
+      "🍌": 60,
+      "🍆": 100
+    },
+    {
+      year: 2020,
+      "🥑": 50,
+      "🍌": 80,
+      "🍆": 120
+    }
+  ])
+
   const [keys, setKeys] = useState(allKeys);
 
   return (
     <div className="container">
 
       <h2>Stacked Bar Chart with D3 </h2>
+      <StackedAreaChart data={data} keys={keys} colors={colors} />
       <StackedBarChart data={data} keys={keys} colors={colors} />
-
       <div className="fields">
         {allKeys.map(key => (
           <div key={key} className="field">
@@ -74,6 +77,22 @@ const App = () => {
           </div>
         ))}
       </div>
+
+      <button
+        onClick={() =>
+          setData([
+            ...data,
+            {
+              year: Math.max(...data.map(d => d.year)) + 10,
+              "🥑": Math.round(Math.random() * 100),
+              "🍌": Math.round(Math.random() * 125),
+              "🍆": Math.round(Math.random() * 150)
+            }
+          ])
+        }
+      >
+        Add data
+      </button>
     </div>
   );
 }
